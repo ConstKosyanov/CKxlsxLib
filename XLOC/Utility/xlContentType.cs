1adf6fbb1e77363b651d052607527bc7218ca4d0
ConstKosyanov <constkosyanov@gmail.com>
2016-04-13 14:47:38 +0300

    Rename directory

---
+++ XLOC/Utility/xlContentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace XLOC.Utility
+{
+    public enum xlContentType : byte
+    {
+        Void = 0,
+        Boolean = 1,
+        Integer = 2,
+        Double = 3,
+        SharedString = 4,
+        String = 5,
+        Date = 6,
+    }
+}
